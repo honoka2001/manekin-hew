dc2f0b8b5eda98d2d81d6d3e1276b399d7416ec4
honoka2001 <honoka519pk@gmail.com>
2022-01-21 18:37:57 +0900

    add : LoginUserFieldコンポーネント

---
+++ front/app/components/Auth/LoginUserField.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export default function LoginUserField() {
+    const [user, setUser] = useState([]);
+
+    useEffect(() => {
+        getLoginUser();
+    }, []);
+
+    const getLoginUser = () => {
+        axios
+            .get('http://localhost:3000/logged_in', { withCredentials: true })
+            .then((res) => {
+                console.log(res);
+                setUser(res.data.user);
+            })
+            .catch((data) => {
+                console.log(data);
+                setUser({});
+            });
+    };
+
+    return (
+        <div>
+            <p>ログインユーザー</p>
+            <h2>メールアドレス</h2>
+            <p>{user.email}</p>
+        </div>
+    );
+}
