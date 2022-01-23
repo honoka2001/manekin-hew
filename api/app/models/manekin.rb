7676e3789738cc12f994e6408fb5a53a0f712fc7
honoka2001 <honoka519pk@gmail.com>
2022-01-23 13:58:24 +0900

    add : Avatar,ManekinImageのUploaderの設定

--- api/app/models/manekin.rb
+++ api/app/models/manekin.rb
@@ -1,4 +1,5 @@
 class Manekin < ApplicationRecord
+  mount_uploader :image, ManekinImageUploader
   belongs_to :user
   belongs_to :buyer, class_name: 'User', foreign_key: :buyer_id
   has_many :items
--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    mount_uploader :image, AvatarUploader
 	has_secure_password
 
     has_many :items, dependent: :destroy
