a3c666d3b7c74869aefb8fbb51b14b93acaab3e3
honoka2001 <honoka519pk@gmail.com>
2022-01-23 13:51:36 +0900

    add : Userモデルにカラム追加

---
+++ api/db/migrate/20220123023138_add_details_to_users.rb
@@ -0,0 +1,7 @@
+class AddDetailsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :introduction, :string
+    add_column :users, :avatar, :string
+    add_column :users, :name, :string
+  end
+end
