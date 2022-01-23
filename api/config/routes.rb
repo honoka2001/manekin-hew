f80cc65bc9d8c75b12ee5c471026f8e85ccb2c20
honoka2001 <honoka519pk@gmail.com>
2022-01-23 20:40:42 +0900

    fix : deleteアクションをdestroyアクションに変更

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   get '/login', to: 'sessions#show'
-  delete '/logout', to: 'sessions#delete'
+  delete '/logout', to: 'sessions#destroy'
 end
