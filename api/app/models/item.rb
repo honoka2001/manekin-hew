5afc43584858e8ae7f5daecf00479061c36e007b
honoka2001 <honoka519pk@gmail.com>
2022-01-24 18:46:29 +0900

    add : optional: true

--- api/app/models/item.rb
+++ api/app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
   mount_uploader :image, ItemImageUploader
   belongs_to :user
-  belongs_to :manekin
+  belongs_to :manekin, optional: true
 end
--- api/app/models/manekin.rb
+++ api/app/models/manekin.rb
@@ -1,6 +1,6 @@
 class Manekin < ApplicationRecord
   mount_uploader :image, ManekinImageUploader
   belongs_to :user
-  belongs_to :buyer, class_name: 'User', foreign_key: :buyer_id
+  belongs_to :buyer, class_name: 'User', foreign_key: :buyer_id, optional: true
   has_many :items
 end
