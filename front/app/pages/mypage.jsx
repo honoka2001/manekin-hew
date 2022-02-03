34ed4c178bdc7b6598adffd82924323c15ae9a77
honoka2001 <honoka519pk@gmail.com>
2022-02-04 01:39:28 +0900

    fix : LogoutButton移動による修正

--- front/app/pages/mypage.jsx
+++ front/app/pages/mypage.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import LoginUserField from '../components/Auth/LoginUserField';
-import LogoutButton from '../components/Auth/LogoutButton';
 
 export default function mypage() {
     return (
         <div>
             <h1>マイページ</h1>
             <LoginUserField />
-            <LogoutButton />
         </div>
     );
 }
