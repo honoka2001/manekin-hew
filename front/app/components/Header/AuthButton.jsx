4a3da67997c1b4a64e20c164799e6d31a40e6e16
Yoshito-4410 <ttymah@icloud.com>
2022-02-04 16:51:35 +0900

    add : ログインページと新規登録ページを分割

--- front/app/components/Header/AuthButton.jsx
+++ front/app/components/Header/AuthButton.jsx
@@ -5,10 +5,10 @@ import styles from '../../styles/Header.module.css';
 export default function AuthButton() {
     return (
         <div className={styles.btn_area}>
-            <Link href="/auth">
+            <Link href="/auth/login">
                 <a className={styles.btn_login}>ログイン</a>
             </Link>
-            <Link href="/auth">
+            <Link href="/auth/registration">
                 <a className={styles.btn_sign_up}>新規登録</a>
             </Link>
         </div>
