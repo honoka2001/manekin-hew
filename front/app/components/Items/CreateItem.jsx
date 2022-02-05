91bb41ec922371bbf7403b1f62037a2c8c512836
honoka2001 <honoka519pk@gmail.com>
2022-02-05 16:57:47 +0900

    remove : user_idのフィールド

--- front/app/components/Items/CreateItem.jsx
+++ front/app/components/Items/CreateItem.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react';
 import axios from 'axios';
 
 function CreateItem() {
-    const [user_id, setUserId] = useState();
     const [name, setName] = useState();
     const [image, setImage] = useState();
     const [preview, setPreview] = useState();
@@ -15,7 +14,6 @@ function CreateItem() {
 
     const handleSubmit = () => {
         const file = new FormData();
-        file.append('user_id', user_id);
         file.append('name', name);
         file.append('image', image[0]);
 
@@ -36,22 +34,18 @@ function CreateItem() {
     };
 
     return (
-        <div className='flex justify-center mt-40'>
+        <div className="flex justify-center mt-40">
             <div>
-                <p className="font-bold text-4xl text-center">アイテムを作成</p><br />
+                <p className="font-bold text-4xl text-center">アイテムを作成</p>
+                <br />
                 <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        placeholder="ユーザー"
-                        onChange={(event) => setUserId(event.target.value)}
-                        className='border border-gray-400 rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline my-4'
-                    /><br />
                     <input
                         type="text"
                         placeholder="アイテムの名前"
                         onChange={(event) => setName(event.target.value)}
-                        className='border border-gray-400 rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline my-4'
-                    /><br />
+                        className="border border-gray-400 rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline my-4"
+                    />
+                    <br />
                     <input
                         accept="image/*"
                         multiple
@@ -60,10 +54,17 @@ function CreateItem() {
                             setImage(event.target.files);
                             handlePreview(event);
                         }}
-                        className='border border-gray-400 rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline my-4'
-                    /><br />
-                    <img src={preview} /><br />
-                    <button type='submit' className='bg-red-500 font-bold text-white text-xl py-2 w-full rounded'>送信</button>
+                        className="border border-gray-400 rounded w-full py-2 px-3 text-gray-500 leading-tight focus:outline-none focus:shadow-outline my-4"
+                    />
+                    <br />
+                    <img src={preview} />
+                    <br />
+                    <button
+                        type="submit"
+                        className="bg-red-500 font-bold text-white text-xl py-2 w-full rounded"
+                    >
+                        送信
+                    </button>
                 </form>
             </div>
         </div>
