f48679421d9099b95b1e10623c4f4b31dcf64fd3
Yoshito-4410 <ttymah@icloud.com>
2022-02-16 16:25:26 +0900

    add : プロフィール編集ファイルを作成

---
+++ front/app/pages/user/edit.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styles from '../../styles/profile.module.css';
+
+export default function Edit() {
+    return (
+        <div>
+            <h1 className={styles.title}>プロフィール設定</h1>
+            <div className={styles.image}>
+                <h2 className={styles.imgs}>画像</h2>
+                <div className={styles.iBox}>
+                    <img src="/00.jpg" alt="user" className={styles.img}/>
+                    <label>
+                        画像を選択する
+                        <input type="file" className={styles.file} />
+                    </label>
+                </div>
+            </div>
+            <div className={styles.nBox}>
+                <div className={styles.nickName}>
+                    <h2 className={styles.nick}>ニックネーム</h2>
+                    <input
+                        type="text"
+                        name="nickName"
+                        className={styles.name}
+                        placeholder="ニックネーム"
+                        autoComplete="off"
+                    />
+                </div>
+                <div className={styles.height}>
+                    <h2 className={styles.hTitle}>身長</h2>
+                    <input type="number" name="height" className={styles.hInput} autoComplete="off" />
+                    cm
+                </div>
+            </div>
+            <div className={styles.selfInt}>
+                <h2 className={styles.sTitle}>自己紹介</h2>
+                <div className={styles.textarea}>
+                    <textarea
+                        name="txt"
+                        className={styles.txt}
+                        cols={100}
+                        rows={7}
+                        maxLength={1000}
+                        onkeyup="ShowLength(value);"
+                        placeholder="自己紹介を入力してください。"
+                        autoComplete="off"
+                        defaultValue={''}
+                    />
+                    <p className={styles.length}>0/1000</p>
+                </div>
+            </div>
+            <div className={styles.button}>
+                <input type="submit" className={styles.update} defaultValue="更新する" />
+            </div>
+        </div>
+    );
+}
