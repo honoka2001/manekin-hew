8a6c9aef63a1a934e19e3e6b13535fef0fb8b381
honoka2001 <honoka519pk@gmail.com>
2022-03-10 21:18:44 +0900

    change : MainVisualの背景色を白に変更

--- front/app/components/MainVisual.jsx
+++ front/app/components/MainVisual.jsx
@@ -2,23 +2,24 @@ import React from 'react';
 import Link from 'next/link';
 
 export default function MainVisual() {
-
     return (
-        <div class="pt-20 flex justify-center items-center mx-20 space-x-4">
-            <div>
-                <Link href="/">
-                    <img src="/buy.svg" width="auto" height="auto" alt="buy" />
-                </Link>
-            </div>
-            <div>
-                <Link href="/manekin/createForm">
-                    <img src="/sell.svg" width="auto" height="auto" alt="sell" />
-                </Link>
-            </div>
-            <div>
-                <Link href="/statistic">
-                    <img src="/know.svg" width="auto" height="auto" alt="know" />
-                </Link>
+        <div className='bg-white pb-6 border-b'>
+            <div class="pt-20 flex justify-center items-center mx-20 space-x-4 ">
+                <div>
+                    <Link href="/">
+                        <img src="/buy.svg" width="auto" height="auto" alt="buy" />
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/manekin/createForm">
+                        <img src="/sell.svg" width="auto" height="auto" alt="sell" />
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/statistic">
+                        <img src="/know.svg" width="auto" height="auto" alt="know" />
+                    </Link>
+                </div>
             </div>
         </div>
     );
