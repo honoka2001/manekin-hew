6f65b43691c4c31990c28c8ced3d81ca0835fde2
honoka2001 <honoka519pk@gmail.com>
2022-03-25 21:31:55 +0900

    fix : 非ログインユーザーをログイン画面にリダイレクト

--- front/app/pages/_app.js
+++ front/app/pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
-
+import '../styles/globals.css';
+import { useEffect } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    const router = useRouter();
+
+    useEffect(() => {
+        if (router.pathname === '/auth/login' || router.pathname === '/auth/registration') return;
+        requireLogin();
+    }, [router.pathname]);
+
+    const requireLogin = () => {
+        axios
+            .get('http://localhost:3000/login', { withCredentials: true })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((error) => {
+                console.log(error);
+                router.push('/auth/login');
+            });
+    };
+
+    return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
