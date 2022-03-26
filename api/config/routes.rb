f5893a365e63317398f0a51d5e4bc1a7cdeb9899
honoka2001 <honoka519pk@gmail.com>
2022-03-26 19:39:33 +0900

    fix : インデント修正

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -9,9 +9,7 @@ Rails.application.routes.draw do
 
   resources :manekins
   resources :purchases
-  resources :statistics
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :statistics, only: %i[index]
 
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
